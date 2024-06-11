00b6648ed47d61a71b2f3b8281a4a828fccd4976
admin <yeojm2282@gmail.com>
2024-06-12 05:07:41 +0900

    3번째

--- DAY1/Hellowworld.py
+++
@@ -1,5 +0,0 @@
-print("helloworld")
-print("helloworld")
-print("helloworld")
-print("helloworld")
-print("helloworld")
--- js/hover.js
+++
@@ -1,7 +0,0 @@
-function over(obj) {
-	obj.src="img/005.png";
-}
-
-function out(obj) {
-	obj.src="img/002.png";
-}
